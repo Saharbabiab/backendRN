e271e2d95f31665baa88caa5f31b424163929422
Saharbabiab <117641327+Saharbabiab@users.noreply.github.com>
2024-07-22 19:32:17 +0300

    d

--- server.js
+++ server.js
@@ -7,7 +7,8 @@ const run = async () => {
     const port = process.env.PORT || 3001;
     await mongoose.connect(
       // fix the url
-      process.env.DB_URL || "mongodb://localhost:27017/"
+      process.env.DB_URL ||
+        "mongodb+srv://saharbabian:*****@babi.yefv4yp.mongodb.net/"
     );
     app.listen(port, () => console.log(`Listening on port: ${port}`));
   } catch (err) {
